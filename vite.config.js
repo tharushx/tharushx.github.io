1529c1ef74f967ef14c69adf18d27f8269681772
tharushx <124893155+tharushx@users.noreply.github.com>
2025-03-28 19:27:12 +0530

    Fix: Alternative Vite config for assets

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    open: true
-  },
-  base: '/tharushx.github.io/',
+  base: '',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  server: {
+    open: true
   }
 }); 
